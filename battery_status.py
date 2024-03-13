defb7c5599f381f9273cec41afae656067a2f50c
HaXr <haxr@outlook.dk>
2024-03-13 13:20:29 +0100

    Battery kode

---
+++ battery_status.py
@@ -0,0 +1,99 @@
+from machine import Pin
+import time
+
+
+########################################
+# OWN MODULES
+from adc_sub import ADC_substitute
+
+
+class Battery_Status:
+    #########################################################################
+    # INIT
+    
+    def __init__(self, battery):
+        self.battery = battery
+    
+    
+    ########################################
+    # CONFIGURATION
+
+    max_bat_voltage = 4.2
+    min_bat_voltage = 3.0
+
+
+    ########################################
+    # OBJECTS
+    
+    #battery = self.bat_subadc  # The battery object
+
+
+    ########################################
+    # VARIABLES
+
+    # Previous values
+    bat_pct = None
+    prev_bat_pct = -1                      # The previous battery percentage value
+    buffer = []                            # Opret en tom buffer
+
+    # Resistors
+    resistor1 = 4.70
+    resistor2 = 4.70
+    max_spaendingsdeler_voltage_measurement = 2.07   # Ikke brugt, bare til egen info. Målt ved 4.2V input med 2x 4.7k modstande.
+
+
+    ########################################
+    # FUNCTIONS
+
+    # Beregner den teoretiske maksimale spænding for spændingsdeleren.
+    def calc_spaendingsdeler(U, R1, R2):
+        U_out = (U * R2) / (R1 + R2)
+        return U_out
+
+    max_spaendingsdeler_voltage = calc_spaendingsdeler(max_bat_voltage, resistor1, resistor2)
+    min_spaendingsdeler_voltage = calc_spaendingsdeler(min_bat_voltage, resistor1, resistor2)
+
+    print(f"Spaendingsdeler Max Voltage: {max_spaendingsdeler_voltage}")
+    print(f"Spaendingsdeler Min Voltage: {min_spaendingsdeler_voltage}")
+
+    def get_battery_percentage(self):          # The battery voltage percentage
+        
+        # Beregn procentdelen af batteriets opladning
+        percentage = ((self.battery.read_voltage() - self.min_spaendingsdeler_voltage) / (self.max_spaendingsdeler_voltage - self.min_spaendingsdeler_voltage)) * 100
+        
+        # Sørg for, at procentdelen er inden for intervallet 0% til 100%
+        percentage = max(0, min(100, percentage))
+
+        return percentage                          # Replace with own math. Use function in adc_sub.py
+                                        # Make the result an integer value, and avoid neg. and above 100% values
+
+    def calculate_average_battery(self, window_size, bat_percentage):
+        self.buffer.append(bat_percentage)
+
+        if len(self.buffer) > window_size:
+            self.buffer.pop(0)  # Fjern ældste værdi, hvis bufferen er fyldt
+
+        if not self.buffer:
+            return 0  # Returner 0, hvis bufferen er tom
+        return int(sum(self.buffer) / len(self.buffer))
+
+
+    def get_battery_status(self):
+        bat_pct = self.calculate_average_battery(20, self.get_battery_percentage())
+        
+        # Send data if there is a change (this principle saves power)
+        if bat_pct != self.prev_bat_pct:
+            data_string = str(time.ticks_ms()) + '|' + str(bat_pct) # The data to send. CHANGE IT! (Added the "sensor_id")
+            
+            #print("Battery Pct: " + data_string)
+            
+            
+    #        try:
+    #            en.send(dashboard_mac_address, data_string, False)
+    #        except ValueError as e:
+    #            print("Error sending the message: " + str(e))  
+            
+            # Update the previous values for use next time
+            self.prev_bat_pct = bat_pct
+            return data_string
+            
--- main.py
+++ main.py
@@ -1,1 +1,65 @@
-execfile("smarthome_sensor_hcsr501.py")
+from battery_status import Battery_Status
+#from gps_stuff import GPS_Stuff
+
+########################################
+# OWN MODULES
+from adc_sub import ADC_substitute
+
+
+#import umqtt_robust2 as mqtt
+#import _thread
+
+#from machine import ADC, Pin
+from time import ticks_ms, sleep
+import sys
+
+#########################################################################
+# CONFIGURATION
+
+pin_adc_bat = 32
+
+
+#########################################################################
+# OBJECTS
+
+battery_subadc = ADC_substitute(pin_adc_bat)  # The battery object
+Battery = Battery_Status(battery_subadc)
+# GPS = GPS_Stuff()
+
+
+
+#########################################################################
+# PROGRAM
+
+battery_status_start = ticks_ms()
+battery_status_period_ms = 1000 # 1000ms = 1s
+
+#gps_stuff_start = ticks_ms()
+#gps_stuff_period_ms = 1000
+
+
+while True:
+    try:
+        #------------------------------------------------------
+        # Battery Status
+        
+        if ticks_ms() - battery_status_start > battery_status_period_ms:
+            battery_status_start = ticks_ms()
+            
+            battery_pct = Battery.get_battery_status()
+            print("Battery Pct: " + battery_pct)
+        #------------------------------------------------------
+        # GPS Stuff
+        
+        # if ticks_ms() - gps_stuff_start > gps_stuff_period_ms:
+        #     gps_stuff_start = ticks_ms()
+            
+        #     print(GPS.get_adafruit_gps())
+        
+    
+
+    except KeyboardInterrupt:
+        print('Ctrl-C pressed...exiting')
+        #mqtt.c.disconnect()
+        sys.exit()
+        
